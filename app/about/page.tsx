616a168ec90201c6a9703813371279e8fe1dff3f
Arismonx <arismonx@gmail.com>
2024-11-18 17:41:36 +0700

    Fetch Data API in about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+const url = "https://jsonplaceholder.typicode.com/todos";
 
-const About = () => {
-  return (
-    <div>
-        <h1>About</h1>
-    </div>
-  )
+const fetchTodos = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    return data;
 }
 
-export default About
+const AboutPage = async () => {
+    const data = await fetchTodos()
+    console.log(data)
+    return (
+        <div>
+            <h1>About</h1>
+            {
+                data.map((item:any,index:number)=>{
+                    return <li key={index}>{item.title}</li>
+                })
+            }
+        </div>
+    )
+}
+export default AboutPage
