408e42421fb13915cd9874355f847a8a60e2bbeb
Arismonx <arismonx@gmail.com>
2024-11-18 19:22:58 +0700

    useFormStatus

--- app/coponents/form/page.tsx
+++ app/coponents/form/page.tsx
@@ -1,15 +1,30 @@
 'use client'
 import { createCamp } from '@/utils/actions'
 import { useActionState } from 'react'
+import { useFormStatus } from 'react-dom'
 
+
+const SubmitButton = () => {
+    const { pending } = useFormStatus()
+    return (
+        <button
+            type='submit'
+            disabled={pending}
+        >
+            {
+                pending ? 'Summiting...' : 'Submit'
+            }
+        </button>
+    )
+}
 const Form = () => {
-    const [message, formAction] = useActionState(createCamp,null)
+    const [message, formAction] = useActionState(createCamp, null)
 
     return (
         <div>
             {message && <p>{message}</p>}
             <h1>Form</h1>
-            <form action={ formAction }>
+            <form action={formAction}>
                 <input
                     className='border border-gray-300 rounded-md p-2'
                     placeholder="Camp Title"
@@ -23,7 +38,7 @@ const Form = () => {
                     className='border border-gray-300 rounded-md p-2'
                     defaultValue="HOME"
                 />
-                <button type="submit">Submit</button>
+                <SubmitButton />
             </form>
 
         </div>
--- utils/actions.tsx
+++ utils/actions.tsx
@@ -3,6 +3,7 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 export const createCamp = async(prevState,formData) => {
+    await new Promise((resolve) => setInterval(resolve,1000))
     // const title = formData.get('title')
     // const location = formData.get('location')
     const rawData = Object.fromEntries(formData)
